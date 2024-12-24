b3605e1069e0f7fe65d573f595a81dee439630cc
moustafa shousha <moustafa.ahmed760@gmail.com>
2024-12-24 19:23:15 +0200

    init commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('OMDB-API ')
+    .setDescription('Movies API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('omdb-api/docs', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
